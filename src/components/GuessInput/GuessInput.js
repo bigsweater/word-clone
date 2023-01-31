c0faaf5ed338ec44970c7a10b69db9c5d00640bc
Vincent Maglione <vincent@bluebench.co>
2023-01-31 17:08:02 -0500

    GuessInput component

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+function GuessInput() {
+    const [guess, setGuess] = React.useState('');
+
+    function handleSubmit(event, guess) {
+        event.preventDefault();
+
+        if (guess.length !== 5) {
+            return;
+        }
+
+        console.log(guess);
+
+        setGuess('');
+    }
+
+    return (
+        <form className="guess-input-wrapper" onSubmit={(e) => handleSubmit(e, guess)}>
+            <label htmlFor="guess-input">Enter your guess:</label>
+            <input
+                type="text"
+                value={guess}
+                id="guess-input"
+                minLength={5}
+                maxLength={5}
+                onChange={(event) => {
+                    const guess = event.target.value.toUpperCase().replace(/[^A-Z]/g, '')
+
+                    setGuess(guess)
+                }
+                } />
+        </form>
+    );
+}
+
+export default GuessInput;
