a8a9b0043367a4dd930591075aff27ea4a8dd33a
Vincent Maglione <vincent@bluebench.co>
2023-01-31 23:08:57 -0500

    Style keyboard

--- src/components/VirtualKeyboard/VirtualKeyboard.js
+++ src/components/VirtualKeyboard/VirtualKeyboard.js
@@ -1,7 +1,25 @@
 import React from "react";
 
+import { KEYBOARD_LETTERS } from '../../data';
+
 function VirtualKeyboard() {
-  return <div />;
+    let initialLetters = {};
+    KEYBOARD_LETTERS.forEach(letter => {
+        initialLetters[letter] = { status: '' }
+    });
+    const [letters, setLetters] = React.useState(initialLetters)
+
+    return (
+        <div className="virtual-keyboard">
+            {Object.keys(letters).map(letter => (
+                <span className={
+                    letters[letter].status
+                        ? `keycap ${letters[letter].status}`
+                        : `keycap`
+                } key={letter}>{letter}</span>
+            ))}
+        </div>
+    );
 }
 
 export default VirtualKeyboard;
--- src/data.js
+++ src/data.js
@@ -50,3 +50,32 @@ export const WORDS = [
   'ROYAL',
   'HONEY',
 ];
+
+export const KEYBOARD_LETTERS = [
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+    'M'
+]
